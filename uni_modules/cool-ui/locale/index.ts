10544fba38c72df391914b675b4c19e771e26f9f
icssoa <615206459@qq.com>
2025-08-28 09:42:22 +0800

    更新语言包

--- uni_modules/cool-ui/locale/index.ts
+++ uni_modules/cool-ui/locale/index.ts
@@ -3,17 +3,17 @@ import { appendLocale } from "@/locale";
 import zhcn from "./zh-cn.json";
 import en from "./en.json";
 import zhtw from "./zh-tw.json";
+import ko from "./ko.json";
+import fr from "./fr.json";
 import es from "./es.json";
 import ja from "./ja.json";
-import fr from "./fr.json";
-import ko from "./ko.json";
 
 setTimeout(() => {
 	appendLocale("zh-cn", zhcn);
 	appendLocale("en", en);
 	appendLocale("zh-tw", zhtw);
+	appendLocale("ko", ko);
+	appendLocale("fr", fr);
 	appendLocale("es", es);
 	appendLocale("ja", ja);
-	appendLocale("fr", fr);
-	appendLocale("ko", ko);
 }, 0);
